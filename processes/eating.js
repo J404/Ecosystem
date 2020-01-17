a98a81b55f26660b914e44a8d6b18e4a472fc59e
Jimmy <jimmystry@gmail.com>
2020-01-17 16:28:35 -0500

    fixed formatting issue

--- processes/eating.js
+++ processes/eating.js
@@ -2,16 +2,16 @@
 // if it is in range, the creature will eat the food
 const checkEdible = (foodPiece, pos, radius) => {
 
-    // Checks if the food is in range
-    const dist = p5.Vector.sub(foodPiece.pos, pos).mag();
-    if (dist < radius / 2) {
+  // Checks if the food is in range
+  const dist = p5.Vector.sub(foodPiece.pos, pos).mag();
+  if (dist < radius / 2) {
 
-      // Delete the food from the food array
-      food.splice(food.indexOf(foodPiece), 1);
+    // Delete the food from the food array
+    food.splice(food.indexOf(foodPiece), 1);
 
-      return true;
-    }
-
-    // If we cant eat it, return false
-    return false;
+    return true;
   }
+
+  // If we cant eat it, return false
+  return false;
+}
